60672a2de1f4e443e504f05080fe488374645301
rogeriodossantoss <78703375+rogeriodossantoss@users.noreply.github.com>
2023-09-23 07:19:34 -0400

    second commit

---
+++ node/utils.js
@@ -0,0 +1,4 @@
+function createLink(file) {
+  return `<a href="dir/${file}">${file}</a>`;
+}
+module.exports = createLink
---
+++ nodeES/index.js
@@ -0,0 +1,26 @@
+import http from "http";
+import dotenv from "dotenv";
+import * as utils from "./utils.js";
+dotenv.config();
+
+const server = http.createServer((req, res) => {
+  const dir = process.argv[2];
+  if (dir !== "dir") {
+    utils.sendResponse(res, 401, "text/html;charset=utf-8", "Folder not found");
+    return;
+  }
+
+  if (req.url === "/") {
+    utils.handleHomePage(dir, res);
+  } else if (req.url === "/favicon.ico") {
+    utils.sendResponse(res, 404, "text/html;charset=utf-8", "File not found");
+  } else {
+    const filePath = "." + req.url;
+    utils.handleFileRequest(filePath, res);
+  }
+});
+
+const PORT = process.env.PORT ?? 5551;
+server.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
